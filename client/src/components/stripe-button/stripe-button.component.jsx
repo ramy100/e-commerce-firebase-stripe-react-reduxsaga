b3df5202ee9b9ead6f399b4d11655c67a1e2ad1c
ramy2020 <ramy.alashmawy@outlook.com>
2020-07-20 22:26:20 +0200

    creating a simple node backend for payment using stripe and separating frontend in the client folder  and preparing it for production

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 function StripeButton({ price }) {
   const priceInCents = price * 100;
   const PublishableKey =
     "pk_test_51GrsV1LymTQ7NsPWaTS9k73M6VmRRiTFXdU48adSFfGyzx9LS17e3VhORhq5KqFq2G85qkCuUE9O7iItpuL8anex00vd84oO7K";
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successfull");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceInCents,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successfull");
+      })
+      .catch((error) => {
+        console.log("payment error");
+        alert("Payment Issue");
+      });
   };
   return (
     <StripeCheckout
