d21ac6d1f5f702dca8df72f49883501fd7854afa
ramy100 <ramy.alashmawy@outlook.com>
2021-02-28 06:00:13 +0200

    small fix

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,34 @@
-import React from "react";
-import StripeCheckout from "react-stripe-checkout";
-import axios from "axios";
-import swal from "sweetalert";
-function StripeButton({ price }) {
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+import swal from 'sweetalert';
+import dotenv from 'dotenv';
+import { clearCart as clearCartAction } from '../../redux/cart/cart.actions';
+import { connect } from 'react-redux';
+dotenv.config();
+
+function StripeButton({ price, clearCart }) {
   const priceInCents = price * 100;
-  const PublishableKey =
-    "pk_test_51GrsV1LymTQ7NsPWaTS9k73M6VmRRiTFXdU48adSFfGyzx9LS17e3VhORhq5KqFq2G85qkCuUE9O7iItpuL8anex00vd84oO7K";
+  const PublishableKey = process.env.REACT_APP_STRIPE_PUBLIC_KEY || '';
   const onToken = (token) => {
     axios({
-      url: "payment",
-      method: "post",
+      url: 'payment',
+      method: 'post',
       data: {
         amount: priceInCents,
         token,
       },
     })
       .then((response) => {
-        swal("Paymen Recieved", "Thanks for shopping!", "success");
+        swal('Paymen Recieved', 'Thanks for shopping!', 'success');
+        clearCart();
       })
       .catch((error) => {
-        console.log("payment error");
+        console.log('payment error');
         swal(
-          "Paymen Rejected",
-          "There was an error recieving payments!",
-          "error"
+          'Paymen Rejected',
+          'There was an error recieving payments!',
+          'error'
         );
       });
   };
@@ -42,4 +47,8 @@ function StripeButton({ price }) {
   );
 }
 
-export default StripeButton;
+const mapDispatchToProps = (dispatch) => ({
+  clearCart: () => dispatch(clearCartAction()),
+});
+
+export default connect(null, mapDispatchToProps)(StripeButton);
--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import env from 'dotenv';
+env.config();
 const config = {
-  apiKey: '**REMOVED**',
-  authDomain: '**REMOVED**',
-  databaseURL: '**REMOVED**',
-  projectId: '**REMOVED**',
-  storageBucket: '**REMOVED**',
-  messagingSenderId: '**REMOVED**',
-  appId: '**REMOVED**',
-  measurementId: '**REMOVED**',
+  apiKey: process.env.REACT_APP_FIRE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIRE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIRE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRE_APP_ID,
+  measurementId: process.env.REACT_APP_FIRE_MEASUREMENT_ID,
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
